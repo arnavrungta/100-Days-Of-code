1055ece5db53be15e14050f2be04389a8702b783
arnavrungta <arnavrungta7@gmail.com>
2025-08-24 09:17:32 +0530

    Add files via upload

---
+++ Day4.Profram2.c
@@ -0,0 +1,17 @@
+
+//Write a program to find and display the sum of the first n natural numbers.
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    int sum=0;
+    for (int i = 1; i <=n; i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
